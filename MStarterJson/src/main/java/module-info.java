51e668e528b003c579cfda22b6cbd3a3d364b61c
Leanfe <l.kapitman@gmail.com>
2022-12-07 20:29:57 +0200

    Fix module

--- MStarterJson/src/main/java/module-info.java
+++ MStarterJson/src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module MStarterJson {
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires lombok;
+
     exports com.github.leanfe.json;
 }
