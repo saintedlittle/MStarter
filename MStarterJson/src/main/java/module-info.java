b86026d3d7bbbfbcc93db5217a2bc4392a984f2b
Leanfe <l.kapitman@gmail.com>
2022-12-08 06:58:55 +0200

    Скажи спасибо богу.

--- MStarterJson/src/main/java/module-info.java
+++ MStarterJson/src/main/java/module-info.java
@@ -3,7 +3,10 @@ module MStarterJson {
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
+    
     requires lombok;
+
+    requires MStarterAPI;
 
     exports com.github.leanfe.json;
 }
