04e047aaa8cb9cf8b9a51b7c4f038f8c8c504934
Leanfe <l.kapitman@gmail.com>
2022-12-08 11:06:54 +0200

    Орки?

--- MStarterAPI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module MStarterAPI {
-    exports com.github.leanfe.api;
-    exports com.github.leanfe.api.json;
-    exports com.github.leanfe.api.exceptions;
-
-    exports com.github.leanfe.api.version.forge;
-    exports com.github.leanfe.api.version.vanilla;
-}
